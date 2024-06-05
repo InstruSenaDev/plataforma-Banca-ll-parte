22e8ded3e64021b539eeb9dee334e844da67fa87
ange <angelicacb6392@gmail.com>
2024-06-05 16:27:38 -0500

    Se realiza Avance del frontend de cajero principal
    Se organiza el responsive y se organiza los contendores superiores

--- simulador-banca-main/Frontend/src/Components/Card/Card.jsx
+++ simulador-banca-main/Frontend/src/Components/Card/Card.jsx
@@ -8,14 +8,14 @@ const Card = () => {
     <div className="flex flex-wrap -mx-3">
       {/* row 1 */}
       {/* card1 */}
-      <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/5">
+      <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
         <div className="relative flex flex-col min-w-0 break-words bg-DarkSlate shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
           <div className="flex-auto p-4">
             <div className="flex flex-row -mx-3 items-center">
               <div className="flex-none w-2/3 max-w-full px-3">
                 <div>
                   <div className="mb-2 w-12 h-12 text-center bg-beige from-blue-500 to-violet-500 rounded-md">
-                    <i className="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"></i>
+                    <i className="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"><box-icon name='credit-card' color='#0a8e4d' ></box-icon></i>
                   </div>
                   <p className="mb-0 font-sans text-sm font-semibold leading-normal uppercase text-white dark:opacity-60">
                     Saldo Total
@@ -31,14 +31,14 @@ const Card = () => {
       </div>
 
       {/* card2 */}
-      <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/5">
+      <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
         <div className="relative flex flex-col min-w-0 break-words bg-darkGray shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
           <div className="flex-auto p-4">
             <div className="flex flex-row -mx-3 items-center">
               <div className="flex-none w-2/3 max-w-full px-3">
                 <div>
                   <div className="mb-2 w-12 h-12 text-center bg-beige from-blue-500 to-violet-500 rounded-md">
-                    <i className="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"></i>
+                    <i className="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"><box-icon name='credit-card-alt' color='#0a4f8e' ></box-icon></i>
                   </div>
                   <p className="mb-0 font-sans text-sm font-semibold leading-normal uppercase text-white dark:opacity-60">
                     Saldo T. en Bóveda
@@ -54,18 +54,18 @@ const Card = () => {
       </div>
 
       {/* card3 */}
-      <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/5">
-  <div className="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
-    <div className="flex-auto p-4">
+      <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+  <div className="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border h-full"> {/* Añadí la clase h-full aquí */}
+  <div className="flex-auto p-4 flex flex-col justify-center items-center"> 
       <div className="flex flex-row -mx-3 items-center">
-        <div className="flex-none w-2/3 max-w-full px-3">
+        <div className="flex-none w-full max-w-full px-3">
           {/* Aquí están los botones */}
           <div className="flex flex-col">
-            <button className="mb-2 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 mb-2 rounded">
-              Botón Verde
+            <button className="mb-2 bg-red hover:bg-red-600 text-white font-bold py-2 px-6 rounded-lg w-full flex items-center justify-center">
+            <box-icon name='wallet' color='#ffffff' ></box-icon> Retirar de Bóveda
             </button>
-            <button className="mb-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-              Botón Rojo
+            <button className="bg-DarkSlate hover:bg-green-600 text-white font-bold py-2 px-6 rounded-lg w-full flex items-center justify-center">
+            <box-icon name='wallet' color='#ffffff' ></box-icon> Consignar a Bóveda
             </button>
           </div>
         </div>
--- simulador-banca-main/Frontend/src/Components/Sidebar/Sidebar.jsx
+++ simulador-banca-main/Frontend/src/Components/Sidebar/Sidebar.jsx
@@ -5,8 +5,9 @@ import "boxicons";
 const Sidebar = () => {
   return (
     <aside
-      className="bg-greenlight inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-50 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0"
+      className="bg-Greenlight inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-50 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0"
       aria-expanded="false"
+      style={{ height: "calc(100vh - 4rem)" }} 
     >
       <div className="h-19">
         <i
