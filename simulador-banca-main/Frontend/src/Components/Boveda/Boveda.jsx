e20e3328b1291b56191d0ab7f3dace543fc8a3b2
Ange6506 <160287988+Ange6506@users.noreply.github.com>
2024-06-14 09:35:35 -0500

    Se arregla el responsive de la Bóveda
    Se mejoró la adaptabilidad del diseño de la Bóveda.

--- simulador-banca-main/Frontend/src/Components/Boveda/Boveda.jsx
+++ simulador-banca-main/Frontend/src/Components/Boveda/Boveda.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import inicio2 from "../../assets/Img/Login/Inicio2.png";
 import "boxicons";
-
+//max-w-1/20 sm:max-w-1/4 h-auto
 const Boveda = () => {
   return (
     <div className="max-w-2x2 px-8 py-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
       <div className="flex flex-col sm:flex-row items-center justify-between">
-      <div className="w-full sm:w-1/2 flex justify-center mb-4 sm:mb-0">
-    <img src={inicio2} alt="" className="max-w-1/8 sm:max-w-1/6 h-auto" />
-</div>
+        <div className="w-full sm:w-[40%] mb-4 sm:mb-0">
+          <img src={inicio2} alt="" className="object-cover w-full" />
+        </div>
+
 
 
         <div className="w-full sm:w-1/2">
@@ -23,9 +24,9 @@ const Boveda = () => {
                         <i className="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"><box-icon name='credit-card' color='#0a8e4d' ></box-icon></i>
                       </div>
                       <p className="mb-0 font-sans text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl font-semibold leading-normal uppercase text-white dark:opacity-60">
-                      Saldo Total de Bóveda                      </p>
+                        Saldo Total de Bóveda                      </p>
                       <h5 className="mb-0 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-white">
-                      99.900.000,00
+                        99.900.000,00
                       </h5>
                     </div>
                   </div>
@@ -34,18 +35,18 @@ const Boveda = () => {
             </div>
           </div>
 
-         <div className="mb-4 ml-auto mr-0">
-         <button className="w-11/12 mt-4 flex items-center justify-center px-3 py-1.5 bg-transparent border border-emerald text-emerald rounded-md focus:outline-none focus:bg-blue-600 mx-6">
-         <box-icon name='wallet' color='emerald'className="mr-2"></box-icon>
-  <span>Consignar</span>
-</button>
-<button className="w-11/12 mt-4 flex items-center justify-center px-3 py-1.5 bg-transparent border border-red text-red rounded-md focus:outline-none focus:bg-blue-600 mx-6">
-  <box-icon name='wallet' color='red' className="mr-2"></box-icon>
-  <span>Retirar</span>
-</button>
+          <div className="mb-4 ml-auto mr-0">
+            <button className="w-11/12 mt-4 flex items-center justify-center px-3 py-1.5 bg-transparent border border-emerald text-emerald rounded-md focus:outline-none focus:bg-blue-600 mx-6">
+              <box-icon name='wallet' color='emerald' className="mr-2"></box-icon>
+              <span>Consignar</span>
+            </button>
+            <button className="w-11/12 mt-4 flex items-center justify-center px-3 py-1.5 bg-transparent border border-red text-red rounded-md focus:outline-none focus:bg-blue-600 mx-6">
+              <box-icon name='wallet' color='red' className="mr-2"></box-icon>
+              <span>Retirar</span>
+            </button>
 
 
-     </div>
+          </div>
         </div>
       </div>
     </div>
--- simulador-banca-main/Frontend/src/Components/Boveda/Transactions.jsx
+++ simulador-banca-main/Frontend/src/Components/Boveda/Transactions.jsx
@@ -6,7 +6,7 @@ const Transactions = () => {
   return (
     <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-8"> {/* Contenedor de las dos cajas */}
       {/* Primera caja */}
-      <div className="flex-1 max-w-2xl px-4 py-3 bg-white border border-emerald rounded-md shadow-md dark:bg-gray-800">
+      <div className="flex-1 max-w-2x2 px-4 py-3 bg-white border border-emerald rounded-md shadow-md dark:bg-gray-800">
         <div className="flex items-center justify-between">
           <span className="text-2xl font-medium text-emerald dark:text-emerald">Ultima Consignacion</span>    
           <span className="px-3 py-1 text-xs text-blue-800 uppercase bg-emerald rounded-full dark:bg-blue-300 text-white">11109757</span>
@@ -26,17 +26,11 @@ const Transactions = () => {
     <h1 class="mt-2 text-gray-700 dark:text-gray-200">Eduardo Dutra</h1>
     <h1 class="mt-2 text-gray-700 dark:text-gray-200"># 0001</h1>
 </div>
-
-
-
-
-  
 </div>
-
       </div>
 
       {/* Segunda caja */}
-      <div className="flex-1 max-w-2xl px-4 py-3 bg-white border border-red rounded-md shadow-md dark:bg-gray-800">
+      <div className="flex-1 max-w-2x2 px-4 py-3 bg-white border border-red rounded-md shadow-md dark:bg-gray-800">
         {/* Contenido de la segunda caja */}
         <div className="flex items-center justify-between">
           <span className="text-2xl font-medium text-red dark:text-red">Ultimo Retiro</span>    
