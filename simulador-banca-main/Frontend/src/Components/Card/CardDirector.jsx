4972e91a0bf8ff4046c443cbc595de94bbeacfe8
Ange6506 <160287988+Ange6506@users.noreply.github.com>
2024-06-20 07:14:48 -0500

    Update CardDirector.jsx

--- simulador-banca-main/Frontend/src/Components/Card/CardDirector.jsx
+++ simulador-banca-main/Frontend/src/Components/Card/CardDirector.jsx
@@ -8,9 +8,9 @@ const CardDirector = () => {
       <div className="w-full h-48 max-w-full px-3 mb-6 sm:flex-none xl:mb-0">
         <div className="h-full relative flex flex-col min-w-0 break-words bg-DarkSlateGray shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
           <div className="flex-auto p-4">
-            <div className="flex flex-row -mx-3 items-center">
-              <div className="flex-none w-2/3 h-full max-w-full px-3">
-                <div>
+            <div className="flex flex-row -mx-3 items-center w-full h-full align-bottom	">
+              <div className="flex-none w-full h-full max-w-full px-3 align-bottom	flex flex-col justify-center">
+                <div className="">
                   <div className="mb-2 w-12 h-12 text-center bg-beige from-blue-500 to-violet-500 rounded-md">
                     <i className="ni leading-none ni-money-coins text-lg relative top-3.5 text-white">
                       <box-icon name='credit-card' color='#325259'></box-icon>
