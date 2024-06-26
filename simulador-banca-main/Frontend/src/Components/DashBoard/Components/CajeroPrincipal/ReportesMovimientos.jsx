d9d8db131ed22508c4122617fefd209991d5e375
Stebanm <juanesxde16@gmail.com>
2024-06-26 16:35:45 -0500

    Se termina tabla movimientos (sin funcionalidad)

--- simulador-banca-main/Frontend/src/Components/DashBoard/Components/CajeroPrincipal/ReportesMovimientos.jsx
+++ simulador-banca-main/Frontend/src/Components/DashBoard/Components/CajeroPrincipal/ReportesMovimientos.jsx
@@ -8,14 +8,14 @@ export const ReportesMovimientos = () => {
           <div className="flex flex-col justify-center items-start">
             <div className="flex flex-row items-center gap-x-3">
               <h2 className="text-lg font-medium text-gray-800 dark:text-white">
-                Autorización de Cuentas
+                Información de movimientos
               </h2>
               <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">
-                0 accounts
+                0 movimientos
               </span>
             </div>
             <p className="text-sm text-gray-500 m-0 p-0">
-              Cuentas pendientes por autorizar
+              Todos los movimientos realizados en el día
             </p>
           </div>
         </div>
@@ -29,69 +29,79 @@ export const ReportesMovimientos = () => {
                     <tr>
                       <th
                         scope="col"
-                        className="py-3.5 px-3 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
+                        className="px-3 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <div className="flex items-center gap-x-3">
-                          <button className="flex justify-center items-center gap-x-2">
+                        <div className="flex justify-center items-center">
+                          <button>
                             <span>ID Movimiento</span>
                           </button>
                         </div>
                       </th>
                       <th
                         scope="col"
-                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
+                        className="px-4 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <div className="flex items-center gap-x-3">
-                          <span>Realizado por</span>
+                        <div className="flex justify-center items-center">
+                          <button>
+                            <span>Realizado por</span>
+                          </button>
                         </div>
                       </th>
 
                       <th
                         scope="col"
-                        className="px-12 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
+                        className="px-6 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-2">
-                          <span>Cliente</span>
-                        </button>
-                      </th>
-
-                      <th
-                        scope="col"
-                        className="px-4 py-3 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
-                      >
-                        <button className="flex items-center gap-x-2">
-                          <span>Saldo</span>
-                        </button>
+                        <div className="flex justify-center items-center">
+                          <button>
+                            <span>Cliente</span>
+                          </button>
+                        </div>
                       </th>
 
                       <th
                         scope="col"
                         className="px-4 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-2">
-                          <span>Fecha y Hora</span>
-                        </button>
+                        <div className="flex justify-center items-center">
+                          <button>
+                            <span>Saldo</span>
+                          </button>
+                        </div>
                       </th>
 
                       <th
                         scope="col"
-                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
+                        className="px-4 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-2">
-                          <span>Acción</span>
-                        </button>
+                        <div className="flex justify-center items-center">
+                          <button>
+                            <span>Fecha y Hora</span>
+                          </button>
+                        </div>
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-6 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
+                      >
+                        <div className="flex justify-center items-center">
+                          <button>
+                            <span>Acción</span>
+                          </button>
+                        </div>
                       </th>
                     </tr>
                   </thead>
                   <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                     <tr>
                       <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                        <div className="inline-flex items-center gap-x-3">
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
                           <span># 0001</span>
                         </div>
                       </td>
                       <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div className="inline-flex items-center gap-x-3">
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
                           <div class="flex items-center gap-x-2">
                             <img
                               class="object-cover w-10 h-10 rounded-full"
@@ -111,29 +121,52 @@ export const ReportesMovimientos = () => {
                       </td>
 
                       <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                        <div class="flex flex-col gap-x-2">
+                        <div class="w-full flex flex-col justify-center items-center gap-x-2">
                           <h2 class="font-medium text-gray-800 dark:text-white ">
                             Juan Esteban
                           </h2>
-                          <p class="text-sm font-normal text-gray-600 dark:text-gray-400">
+                          <p class="text-sm font-normal text-gray-500 dark:text-gray-400">
                             N° 1000000001
                           </p>
                         </div>
                       </td>
 
                       <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                        <div className="inline-flex items-center gap-x-3">
-                          <span>$ 100.000,00</span>
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
+                          <span>$ 100.000.000,00</span>
                         </div>
                       </td>
 
-                      <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-amber-100/60 dark:bg-gray-800">
-                          <span className="h-1.5 w-1.5 rounded-full bg-amber-500"></span>
+                      <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
+                          <span>22/06/2024 9:16:06 am</span>
+                        </div>
+                      </td>
 
-                          <h2 className="text-sm font-normal text-amber-500">
-                            estado
-                          </h2>
+                      <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                        <div className="w-full flex justify-center items-center">
+                          <div className="inline-flex justify-center items-center px-3 py-1 rounded-full gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                            <span className="text-emerald-500">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.3}
+                                stroke="currentColor"
+                                className="size-4"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M12 4.5v15m7.5-7.5h-15"
+                                />
+                              </svg>
+                            </span>
+
+                            <h2 className="text-sm font-normal text-emerald-500">
+                              Consignación
+                            </h2>
+                          </div>
                         </div>
                       </td>
                     </tr>
