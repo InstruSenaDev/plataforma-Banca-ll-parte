8a7f60c4df2a25d16239d54540c21ea9f3777961
Stebanm <juanesxde16@gmail.com>
2024-06-27 08:12:55 -0500

    Se termina tabla movimientos

--- simulador-banca-main/Frontend/src/Components/DashBoard/Components/CajeroPrincipal/ReportesMovimientos.jsx
+++ simulador-banca-main/Frontend/src/Components/DashBoard/Components/CajeroPrincipal/ReportesMovimientos.jsx
@@ -29,20 +29,22 @@ export const ReportesMovimientos = () => {
                     <tr>
                       <th
                         scope="col"
-                        className="py-3.5 px-3 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
+                        className="px-3 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <div className="flex items-center gap-x-3">
-                          <button className="flex justify-center items-center gap-x-2">
+                        <div className="flex justify-center items-center gap-x-3">
+                          <button>
                             <span>ID Movimiento</span>
                           </button>
                         </div>
                       </th>
                       <th
                         scope="col"
-                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <div className="flex items-center gap-x-3">
-                          <span>Realizado por</span>
+                        <div className="flex justify-center items-center gap-x-3">
+                          <button>
+                            <span>Realizado por</span>
+                          </button>
                         </div>
                       </th>
 
@@ -50,48 +52,56 @@ export const ReportesMovimientos = () => {
                         scope="col"
                         className="px-12 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-2">
-                          <span>Cliente</span>
-                        </button>
+                        <div className="flex justify-center items-center gap-x-2">
+                          <button>
+                            <span>Cliente</span>
+                          </button>
+                        </div>
                       </th>
 
                       <th
                         scope="col"
                         className="px-4 py-3 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-2">
-                          <span>Saldo</span>
-                        </button>
+                        <div className="flex justify-center items-center gap-x-2">
+                          <button>
+                            <span>Saldo</span>
+                          </button>
+                        </div>
                       </th>
 
                       <th
                         scope="col"
                         className="px-4 py-4 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-2">
-                          <span>Fecha y Hora</span>
-                        </button>
+                        <div className="flex justify-center items-center gap-x-2">
+                          <button>
+                            <span>Fecha y Hora</span>
+                          </button>
+                        </div>
                       </th>
 
                       <th
                         scope="col"
                         className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-white dark:text-gray-400"
                       >
-                        <button className="flex items-center gap-x-2">
-                          <span>Acción</span>
-                        </button>
+                        <div className="flex justify-center items-center gap-x-2">
+                          <button>
+                            <span>Acción</span>
+                          </button>
+                        </div>
                       </th>
                     </tr>
                   </thead>
                   <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                     <tr>
                       <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                        <div className="inline-flex items-center gap-x-3">
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
                           <span># 0001</span>
                         </div>
                       </td>
-                      <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div className="inline-flex items-center gap-x-3">
+                      <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
                           <div class="flex items-center gap-x-2">
                             <img
                               class="object-cover w-10 h-10 rounded-full"
@@ -111,29 +121,52 @@ export const ReportesMovimientos = () => {
                       </td>
 
                       <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                        <div class="flex flex-col gap-x-2">
+                        <div class="flex flex-col justify-center items-center gap-x-2">
                           <h2 class="font-medium text-gray-800 dark:text-white ">
                             Juan Esteban
                           </h2>
-                          <p class="text-sm font-normal text-gray-600 dark:text-gray-400">
+                          <p class="text-sm font-normal text-gray-500 dark:text-gray-400">
                             N° 1000000001
                           </p>
                         </div>
                       </td>
 
                       <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                        <div className="inline-flex items-center gap-x-3">
-                          <span>$ 100.000,00</span>
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
+                          <span>$ 100.000.000,00</span>
                         </div>
                       </td>
 
-                      <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-amber-100/60 dark:bg-gray-800">
-                          <span className="h-1.5 w-1.5 rounded-full bg-amber-500"></span>
+                      <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
+                        <div className="w-full inline-flex justify-center items-center gap-x-3">
+                          <span>22/06/2024 9:16:06 a. m</span>
+                        </div>
+                      </td>
 
-                          <h2 className="text-sm font-normal text-amber-500">
-                            estado
-                          </h2>
+                      <td className="px-6 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                        <div className="w-full inline-flex justify-center items-center">
+                          <div className="flex justify-center items-center px-3 py-1 rounded-full gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                            <span className="text-emerald-500">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.3}
+                                stroke="currentColor"
+                                className="size-4"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M12 4.5v15m7.5-7.5h-15"
+                                />
+                              </svg>
+                            </span>
+
+                            <h2 className="text-sm font-normal text-emerald-500">
+                              Consignación
+                            </h2>
+                          </div>
                         </div>
                       </td>
                     </tr>
