368f3a295b9e523ac5354fe153ffffc2d7fb4fdf
Stebanm <juanesxde16@gmail.com>
2024-07-31 17:05:21 -0500

    Se crear controlador de registrar movimiento para registrar devolucion de saldo

--- simulador-banca-main/Frontend/src/Components/DashBoard/Components/Director/CrearUsuario.jsx
+++ simulador-banca-main/Frontend/src/Components/DashBoard/Components/Director/CrearUsuario.jsx
@@ -93,6 +93,25 @@ export const CrearUsuario = () => {
           throw new Error("Network response was not ok");
         }
 
+        const responseMovimiento = await fetch(
+          `http://localhost:3000/post_devolver/`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              idEmpleado: userId,
+              saldo: saldoEmpleado,
+              tipoMovimiento: 5,
+            }),
+          }
+        );
+
+        if (!responseMovimiento.ok) {
+          throw new Error("Network response was not ok");
+        }
+
         const response = await fetch(
           `http://localhost:3000/delete_user/${userId}`,
           {
