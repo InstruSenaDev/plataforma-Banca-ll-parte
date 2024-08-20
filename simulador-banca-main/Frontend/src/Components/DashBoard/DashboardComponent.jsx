bf361f9007144ae742e687080ff4ed0145769e03
ange <angelicacb6392@gmail.com>
2024-08-20 13:11:52 -0500

    gestion de consignaciones de cajeros hacia clientes

--- simulador-banca-main/Frontend/src/Components/DashBoard/DashboardComponent.jsx
+++ simulador-banca-main/Frontend/src/Components/DashBoard/DashboardComponent.jsx
@@ -25,6 +25,7 @@ import { ClientView } from "./Components/Cliente/ClientView";
 import { AllTarjets } from "./Components/Cliente/AllTarjets";
 import { ClientMovimientos } from "./Components/Cliente/ClientMovimientos";
 import  { Cancelación } from "./Components/Cajero/Cancelación";
+import   Consignar  from "./Components/Cajero/Consignar";
 
 
 export const DashboardComponent = () => {
@@ -523,10 +524,35 @@ export const DashboardComponent = () => {
                                     Cancelación C. Ahorros
                                   </span>
                                 </button>
+
                               </div>
+                              
                             )}
 
-                           
+<button
+                              className="flex items-center px-4 py-2 font-medium tracking-wide text-darkGray capitalize transition-colors duration-300 transform bg-transparent rounded-md hover:bg-darkGray hover:text-white focus:outline-none space-x-2 w-full xl:text-sm 2xl:text-base"
+                              onClick={() => {
+                                closeSidebar();
+                                handleBotonClick("Consignar");
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                                />
+                              </svg>
+
+                              <span className="mx-1">Consignar</span>
+                            </button>
                           </>
                         )}
 
@@ -767,6 +793,8 @@ export const DashboardComponent = () => {
                   {contenidoSeleccionado === "Boveda" && <Boveda />}
                   {contenidoSeleccionado === "Transfers" && <Transfers />}
                   {contenidoSeleccionado === "Cancelación" && <Cancelación/>}
+                  {contenidoSeleccionado === "Consignar" && <Consignar/>}
+
                   {contenidoSeleccionado === "AperturaCuentaAhorro" && (
                     <AperturaCuentaAhorro />
                   )}
