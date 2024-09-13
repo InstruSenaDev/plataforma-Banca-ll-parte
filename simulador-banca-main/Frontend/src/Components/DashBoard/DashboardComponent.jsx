659995a961479b91e858523c2a3d83a591f81746
Ange6506 <160287988+Ange6506@users.noreply.github.com>
2024-09-13 08:49:33 -0500

    Se hace movimientos

--- simulador-banca-main/Frontend/src/Components/DashBoard/DashboardComponent.jsx
+++ simulador-banca-main/Frontend/src/Components/DashBoard/DashboardComponent.jsx
@@ -558,7 +558,7 @@ export const DashboardComponent = () => {
                               className="flex items-center px-4 py-2 font-medium tracking-wide text-darkGray capitalize transition-colors duration-300 transform bg-transparent rounded-md hover:bg-darkGray hover:text-white focus:outline-none space-x-2 w-full xl:text-sm 2xl:text-base"
                               onClick={() => {
                                 closeSidebar();
-                                handleBotonClick("Consignar");
+                                handleBotonClick("Movimientos");
                               }}
                             >
                               <svg
@@ -576,7 +576,7 @@ export const DashboardComponent = () => {
                                 />
                               </svg>
 
-                              <span className="mx-1">Consignar</span>
+                              <span className="mx-1">Movimientos</span>
                             </button>
                           </>
                         )}
