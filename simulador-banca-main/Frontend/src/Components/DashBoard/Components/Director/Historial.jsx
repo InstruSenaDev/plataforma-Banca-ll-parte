6c23654eae700589baaff64121b6bf03015bf4d3
Luna1808 <160421842+Luna1808@users.noreply.github.com>
2024-10-09 09:53:10 -0500

    Se corrige el responsive de las tablas

--- simulador-banca-main/Frontend/src/Components/DashBoard/Components/Director/Historial.jsx
+++ simulador-banca-main/Frontend/src/Components/DashBoard/Components/Director/Historial.jsx
@@ -156,7 +156,7 @@ export const Historial = () => {
 
         <div className="flex flex-col justify-between flex-1">
           <div className="flex flex-col mt-6">
-            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="-mx-4 -my-2 overflow-x-auto">
               <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                 <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
                   <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
